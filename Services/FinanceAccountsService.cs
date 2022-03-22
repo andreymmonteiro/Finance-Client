d79603b030d455bb0186ba2d2efd84e086a2ad00
andreymmonteiro <andreymuti@hotmail.com>
2022-03-21 22:13:14 -0300

    First commit - Finance accounts service configured, not finished

---
+++ Services/FinanceAccountsService.cs
@@ -0,0 +1,26 @@
+﻿using Financial.Application.Protos;
+using Financial_Client.Services.Interface;
+using System.Threading.Tasks;
+
+namespace Financial_Client.Services
+{
+    public class FinanceAccountsService : IFinanceAccountsService
+    {
+        private readonly Finance.FinanceClient _financeClient;
+
+        public FinanceAccountsService(Finance.FinanceClient financeBase)
+        {
+            _financeClient = financeBase;
+        }
+
+        public async Task<object> GetAllAsync()
+        {
+            return await _financeClient.GetAllFinanceAccountsAsync(new FinanceAccountsRequest());
+        }
+
+        public async Task<object> CreateAsync(FinanceAccountsCreateRequest request) 
+        {
+            return await _financeClient.CreateFinanceAccountsAsync(request);
+        }
+    }
+}
---
+++ Services/Interface/IFinanceAccountsService.cs
@@ -0,0 +1,11 @@
+﻿using Financial.Application.Protos;
+using System.Threading.Tasks;
+
+namespace Financial_Client.Services.Interface
+{
+    public interface IFinanceAccountsService
+    {
+        Task<object> GetAllAsync();
+        Task<object> CreateAsync(FinanceAccountsCreateRequest request);
+    }
+}
